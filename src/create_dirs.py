3351f314f664d413cf720292ca39ede8e326cabb
hernanfuria <105759780+hernanfuria@users.noreply.github.com>
2023-10-27 16:02:08 -0300

    Create empty dirs

--- src/create_dirs.py
+++ src/create_dirs.py
@@ -1,2 +1,17 @@
+from os import mkdir
+from os.path import join, isdir
+from src.env import ROOT_PATH, ASSETS_PATH, QGZ_PATH, SHP_PATH
+
+
 def create_dirs():
-    pass
+    if not isdir(ASSETS_PATH):
+        mkdir(ASSETS_PATH)
+        print(f"{ASSETS_PATH} \033[32mcreated\033[0m")
+
+    if not isdir(QGZ_PATH):
+        mkdir(QGZ_PATH)
+        print(f"{QGZ_PATH} \033[32mcreated\033[0m")
+
+    if not isdir(SHP_PATH):
+        mkdir(SHP_PATH)
+        print(f"{SHP_PATH} \033[32mcreated\033[0m")
---
+++ src/env.py
@@ -0,0 +1,8 @@
+from os.path import join
+from os import getcwd
+
+ROOT_PATH = getcwd()
+
+ASSETS_PATH = join(ROOT_PATH, 'assets')
+QGZ_PATH = join(ASSETS_PATH, 'QGZ')
+SHP_PATH = join(ASSETS_PATH, 'SHP')
