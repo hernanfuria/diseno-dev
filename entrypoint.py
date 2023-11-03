bd53f35b9dcda952da562c7bd49e2babca36af50
hernanfuria <105759780+hernanfuria@users.noreply.github.com>
2023-11-03 19:02:05 -0300

    Added obstacles

--- entrypoint.py
+++ entrypoint.py
@@ -1,4 +1,8 @@
 from src.create_dirs import create_dirs
 
+from src.path_finder import _tests
+
 if __name__ == '__main__':
     create_dirs()
+    _tests()
+
--- src/path_finder.py
+++ src/path_finder.py
@@ -1,5 +1,10 @@
-from shapely.ops import Point, MultiPoint, LineString, MultiLineString
+import geopandas as gpd
+from shapely.ops import Point, MultiPoint, LineString, MultiLineString, GeometryCollection
 from shapely import unary_union
+
+from os.path import join
+
+from src.env import SHP_PATH
 
 
 class Walker:
@@ -11,7 +16,7 @@ class Walker:
             next: list = None,  # list of Walkers
             current_pos: Point = None,
             target: Point = None,
-            bloquers: list = None,  # list of Points
+            obstacles: list = None,  # list of Points
             path: MultiLineString = None,
             step_size: float = 1.0,
             reach_dist: float = 1.0
@@ -21,7 +26,7 @@ class Walker:
         :param next: List of Walker objects that are found after this one
         :param current_pos: shapely Point object, current position of this Walker object
         :param target: shapely Point object, The location where the walker is trying to reach
-        :param bloquers: List of shapely Point objects, obstacles in the path (not implemented yet)
+        :param obstacles: List of shapely Point objects, obstacles in the path (not implemented yet)
         :param path: shapely LineString or MultiLineString object, the path the Walker walks
         :param step_size: Distance between a Walker object and its next one(s)
         :param reach_dist: Maximum distance from the target at which the Walker is considered arrived
@@ -39,7 +44,7 @@ class Walker:
         self.next = next if next is not None else []
         self.current_pos = current_pos
         self.target = target
-        self.bloquers = bloquers if bloquers is not None else []
+        self.obstacles = obstacles if obstacles is not None else []
         self.path = path
         self.step_size = step_size
         self.reach_dist = reach_dist
@@ -78,11 +83,12 @@ class Walker:
         if isinstance(inter, Point):
             if not self._matches_previous(inter):
                 next_steps = [inter]
-        if isinstance(inter, MultiPoint):
+        if isinstance(inter, MultiPoint) or isinstance(inter, GeometryCollection):
             next_steps = []
             for p in list(inter.geoms):
-                if not self._matches_previous(p):
-                    next_steps.append(p)
+                if isinstance(p, Point):
+                    if not self._matches_previous(p):
+                        next_steps.append(p)
 
         # return remaining intersections
         return [
@@ -90,7 +96,7 @@ class Walker:
                 previous=self,
                 current_pos=ns,
                 target=self.target,
-                bloquers=self.bloquers,
+                obstacles=self.obstacles,
                 path=self.path,
                 step_size=self.step_size,
                 reach_dist=self.reach_dist
@@ -108,11 +114,20 @@ class Walker:
             self.target_found = True
             return [self.target_found, [self.current_pos]]
 
+        # base case: if dist(current_pos, obstacle) < step_size, end here (path blocked)
+        for obstacle in self.obstacles:
+            if self.current_pos.distance(obstacle) <= self.step_size:
+                print(f'obstacle at {self}')
+                self.is_path_end = True
+                self.target_found = False
+                return [self.target_found, [self.current_pos]]
+
         # find next step(s)
         self.next = self._find_next_steps()
 
         # base case: if there are no next steps, end here (dead end)
         if self.next == []:
+            print(f'dead end at {self}')
             self.is_path_end = False
             self.target_found = False
             return [self.target_found, [self.current_pos]]
@@ -141,19 +156,46 @@ class Walker:
 
 
 def _tests():
-    path = unary_union([
+    lines = [
         LineString([(0, 0), (10, 0)]),
         LineString([(10, -1), (10, 11)]),
         LineString([(11, 10), (0, 10)]),
-    ])
-    # print(path, type(path))
+        LineString([(5, -1), (5, 11)]),
+    ]
+    starting_point = Point(2, 0)
+    target_point = Point(0, 10)
+    obstacles = [Point(5, 5)]
 
-    w = Walker(current_pos=Point(5, 0), target=Point(5, 10), path=path, step_size=0.1, reach_dist=0.05)
+    path = unary_union(lines)
+
+    w = Walker(
+        current_pos=starting_point,
+        target=target_point,
+        obstacles=obstacles,
+        path=path,
+        step_size=0.1,
+        reach_dist=0.1
+    )
     # for ns in w._find_next_steps():
     #     print(ns)
-    for p in w.walk():
-        print(p)
+    walk = w.walk()
+    if walk is not None:
+        for p in walk:
+            print(p)
 
+        lines_gdf = gpd.GeoDataFrame({'geometry': lines}, crs="EPSG:4326")
+        sp_gdf = gpd.GeoDataFrame({'geometry': [starting_point]})
+        tp_gdf = gpd.GeoDataFrame({'geometry': [target_point]})
+        o_gdf = gpd.GeoDataFrame({'geometry': obstacles})
+        walk_gdf = gpd.GeoDataFrame({'geometry': walk})
+
+        lines_gdf.to_file(join(SHP_PATH, 'lines.shp'))
+        sp_gdf.to_file(join(SHP_PATH, 'start.shp'))
+        tp_gdf.to_file(join(SHP_PATH, 'target.shp'))
+        o_gdf.to_file(join(SHP_PATH, 'obstacles.shp'))
+        walk_gdf.to_file(join(SHP_PATH, 'walk.shp'))
+    else:
+        print('no hay camino')
 
     print("\033[32m _tests executed successfully \033[0m")
 
