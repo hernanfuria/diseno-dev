26ae38fde954ecf97359b21a3cecc304acc4f31c
hernanfuria <105759780+hernanfuria@users.noreply.github.com>
2023-11-29 16:38:06 -0300

    MainThread created
    This class in only meant for simulating the main thread
    
    Signed-off-by: hernanfuria <105759780+hernanfuria@users.noreply.github.com>

---
+++ src/main_thread.py
@@ -0,0 +1,12 @@
+class MainThread:
+    """This class in only meant for simulating the main thread"""
+
+    def __init__(self) -> None:
+        pass
+
+    def run(self):
+        # read data
+        # find/collect paths
+        # create/collect graph
+        # find groups by n
+        pass
